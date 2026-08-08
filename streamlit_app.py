0e6a974c0522df02cdafaa82b8bfe1550306fe3a
ClareciaRose <80193550+ClareciaRose@users.noreply.github.com>
2026-08-08 15:42:12 +0800

    Fix fruit search fallback and update API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,12 @@ if ingredients_list:
         try: 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         except:
-            search_on = 'Could not find fruit.'
+            search_on = 'Apple'
         
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutritien Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
 
