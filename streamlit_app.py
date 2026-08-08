dce819fc85e683feafec66ed1a8e6c86ec528784
ClareciaRose <80193550+ClareciaRose@users.noreply.github.com>
2026-08-08 14:51:56 +0800

    Refactor smoothie order insertion logic
    Removed commented-out code and adjusted smoothie order insertion.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+  
 
-   # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '
@@ -38,9 +40,4 @@ if ingredients_list:
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
         st.success(f'Hi {name_on_order}. Your Smoothie is ordered!', icon="✅")
-
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        
+      
